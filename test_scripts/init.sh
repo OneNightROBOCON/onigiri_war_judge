a1338eb7c43f23aca5f47c85b07368193d17bee1
dashimaki360 <17.punch@gmail.com>
2017-12-03 15:04:58 +0900

    add init.sh

--- test_scripts/init.sh
+++ test_scripts/init.sh
@@ -1,66 +1,58 @@
+#!/bin/bash
+
+# initialize game state
+############# How to use #########################
+# Args {makerset file} {server IP(default localhost:5000)} {red side player name} {blue side player name} 
+
+# {marker set file}
+# {red side player name} ishiro
+# {blue side player name} jiro
+# {server IP} localhost:5000
+
+########## setting #########################
+
+MARKER_SET_FILE=$1
+SERVER_IP=$2
+RED_PLAYER_NAME=$3
+BLUE_PLAYER_NAME=$4
+
+########### script ########################
+
 # reset
 echo "=================RESET======================"
-curl localhost:5000/reset
+curl ${SERVER_IP}/reset
 
 sleep 1
 
 # GET war state initial
 echo "=================STATE initial======================"
-curl localhost:5000/warState
+curl ${SERVER_IP}/warState
+
 sleep 1
 
 # regist targets 
 echo "=================REGIST targets======================"
-curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"name":"one", "id":"1111", "point":3}' localhost:5000/warState/targets
-curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"name":"two", "id":"2222", "point":3}' localhost:5000/warState/targets
-curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"name":"three", "id":"3333", "point":3}' localhost:5000/warState/targets
+# read marker set csv
+for line in `cat ${MARKER_SET_FILE}`
+do
+  NAME=`echo ${line} | cut -d ',' -f 1`
+  POINT=`echo ${line} | cut -d ',' -f 2`
+  ID=`echo ${line} | cut -d ',' -f 3`
+  # print
+  echo ${NAME}
+  echo ${POINT}
+  echo ${ID}
+  echo "{\"name\":${NAME}, \"id\":${ID}, \"point\":${POINT}}"
+  # post target
+  curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d "{\"name\":\"${NAME}\", \"id\":\"${ID}\", \"point\":\"${POINT}\"}" ${SERVER_IP}/warState/targets
+  #curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d ${JSON} ${SERVER_IP}/warState/targets
+  echo
+done
 
 sleep 1
 
 # regist players
 echo "=================REGIST players======================"
-curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"name":"ishiro"}' localhost:5000/warState/players
-curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"name":"jiro"}' localhost:5000/warState/players
+curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d "{\"name\":\"${RED_PLAYER_NAME}\"}" ${SERVER_IP}/warState/players
+curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d "{\"name\":\"${BLUE_PLAYER_NAME}\"}" ${SERVER_IP}/warState/players
 
-#sleep 1
-#
-## ready
-#echo "=================set ready players======================"
-#curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"name":"ishiro", "side":"r", "id":"0000"}' localhost:5000/submits
-#curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"name":"jiro", "side":"b", "id":"0000"}' localhost:5000/submits
-#
-#sleep 1
-#
-## set state to "running"
-#echo "=================set state "running"======================"
-#curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"state":"running"}' localhost:5000/warState/state
-#
-#sleep 1
-#
-#echo "=================STATE running======================"
-#curl localhost:5000/warState
-#
-#sleep 1
-#
-#
-## submit
-#echo "=================submit target id======================"
-#curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"name":"ishiro", "side":"r", "id":"1111"}' localhost:5000/submits
-#curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"name":"ishiro", "side":"r", "id":"2222"}' localhost:5000/submits
-#curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"name":"ishiro", "side":"r", "id":"2222"}' localhost:5000/submits
-#curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"name":"jiro", "side":"b", "id":"2222"}' localhost:5000/submits
-#curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"name":"ishiro", "side":"r", "id":"2223"}' localhost:5000/submits
-#
-#sleep 1
-#
-#
-## set state to "end"
-#echo "=================set state "end"======================"
-#curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"state":"stop"}' localhost:5000/warState/state
-#
-#sleep 1
-#
-#
-#echo "=================STATE end======================"
-#curl localhost:5000/warState
-#
